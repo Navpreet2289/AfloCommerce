d812aab9d76c57357e9f6b2a2b85f6241c672f9f
Michael Logic <ml@michaellogic.com>
2017-10-13 17:39:55 -0500

    Correct Catalog Source fields

--- server/aflo/catalog-source/catalog-source.fields.ts
+++ server/aflo/catalog-source/catalog-source.fields.ts
@@ -1,8 +1,12 @@
-
-export class catalog-source {
-    static fields: Array <any> = [
-        { field: 'title', heading: 'Title', list: true },
-        { field: 'sourcePath', heading: 'Source Path', list: true },
-        { field: 'approved', heading: 'Approved', dataType: 'boolean', list: true }
-    ];
-}
+module.exports = [
+	{ field: 'title', heading: 'Title', list: true },
+    { field: 'sourcePath', heading: 'Source Path', list: true },
+    { field: 'approved', heading: 'Approved', dataType: 'boolean', list: true }
+];
+// export class catalog-source {
+//     static fields: Array <any> = [
+//         { field: 'title', heading: 'Title', list: true },
+//         { field: 'sourcePath', heading: 'Source Path', list: true },
+//         { field: 'approved', heading: 'Approved', dataType: 'boolean', list: true }
+//     ];
+// }
